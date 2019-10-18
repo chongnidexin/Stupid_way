ce18c0802a373166bd87f5dd2f1b9630a3cc5bf0
guohongxin <guohongxin@baice100.com>
2019-10-18 22:52:50 +0800

    参数，解包和变量

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+# script = str(input(111111))
+# first = str(input(11111111))
+# second = str(input(11111))
+# third = str(input(111111111))
+print("The script is called:", script)
+print("Your first variable is :", first)
+print("Your second variable is :", second)
+print("Your third variable is :", third)
