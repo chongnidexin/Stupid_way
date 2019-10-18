f98724dacd18240198365658892f40ee005f0de9
guohongxin <guohongxin@baice100.com>
2019-10-18 22:52:21 +0800

    提示别人

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = str(input("How much do you weigh? "))
+
+
+print("So, you're %r old , %r tall and %r heavy." % (age, height, weight))
